b89ba7641a587e6d9cf02f087e67c22f1fffc553
Maxwell DeMaio <maxdman321@gmail.com>
2020-05-16 20:10:31 -0400

    Refined helper functions

--- helpers.py
+++ helpers.py
@@ -1,36 +1,20 @@
 # Helper functions for our application
 
 # Choose tables to pull verbs, tenses, and conjugations based on the language id
-def tenseTable(lang_id):
+def get_langInfo(lang_id):
     if lang_id == 1:
-        return "en_tense"
+        return {"language": "English", "tenseTable": "en_tense", "verbTable": "en_verb", 
+        "conjTable": "en_conj", "subPronouns": ["I", "You", "He/She/It/One", "We", "You/You all", "They"]}
     else:
-        pass
-
-def verbTable(lang_id):
-    if lang_id == 1:
-        return "en_verb"
-    else:
-        pass
-
-def conjTable(lang_id):
-    if lang_id == 1:
-        return "en_conj"
-    else:
-        pass
-
-def subPronouns(lang_id):
-    if lang_id == 1:
-        return ["I", "You", "He/She/It/One", "We", "You/You all", "They"]
-    else:
-        pass
+        return None
 
 def main():
     class Language:
-        # Allows us to keep track of number of langs, corresponds with lang_id
+
         counter = 1
 
-        def __init__(self, lang_id, tenseTable, verbTable, conjTable, subPronouns):
+        def __init__(self, language, lang_id, tenseTable, verbTable, conjTable, subPronouns):
+            self.language = language
             self.lang_id = lang_id
             self.tenseTable = tenseTable
             self.verbTable = verbTable
@@ -40,17 +24,20 @@ def main():
             Language.counter += 1
 
         def print_info(self):
+            print(f"Language: {self.language}")
             print(f"Language ID: {self.lang_id}")
             print(f"Language Tense Table: {self.tenseTable}")
             print(f"Language Verb Table: {self.verbTable}")
             print(f"Language Conjugation Table: {self.conjTable}")
             print(f"Language Subject Pronouns: {self.subPronouns}")
+            print()
 
-    # Define our instances
-    english = Language(lang_id=1, tenseTable="en_tense", verbTable="en_verb", conjTable="en_conj",
-            subPronouns = ["I", "You", "He/She/It/One", "We", "You/You all", "They"])
+    # Language instances
+    english = Language(lang_id=1, language= "English", tenseTable="en_tense", 
+        verbTable="en_verb", conjTable="en_conj", 
+        subPronouns = ["I", "You", "He/She/It/One", "We", "You/You all", "They"])
 
     english.print_info()
-    
+
 if __name__ == "__main__":
     main()
